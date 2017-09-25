da0ca2a24c4d3a4cd61471b689141337044e7e48
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-09-25 15:11:37 -0400

    Implementando o padrão Adapter.

---
+++ Adapter/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+
+        public Cliente(string nome, string endereco, DateTime dataNasc)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            DataNascimento = dataNasc;
+        }
+
+        public Cliente()
+        {
+
+        }
+
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cliente = new Cliente("Aaaaa", "bbbbbb", DateTime.Now);
+            var clienteXml = ConversorDeXML.GeraXML(cliente);
+            Console.WriteLine(clienteXml);
+            Console.ReadKey();
+        }
+    }
+}
